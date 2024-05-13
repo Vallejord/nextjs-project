526c4bf5736940ce7728eb8b1fdc32a1d7ccf3e0
Vallejord <burlete@hotmail.com>
2024-05-13 17:57:35 -0300

    workin in thinkpad

--- utils/Request.js
+++ utils/Request.js
@@ -19,4 +19,23 @@ async function fetchProperties() {
   }
 }
 
-export { fetchProperties };
+async function fetchProperty(id) {
+  try {
+    //handle the case where domain is not available yet
+    if (!apiDomain) {
+      return null;
+    }
+    const res = await fetch(`${apiDomain}/properties/${id}`);
+
+    if (!res.ok) {
+      throw new Error("Failed to fetch data");
+    }
+
+    return res.json();
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
+
+export { fetchProperties, fetchProperty };
