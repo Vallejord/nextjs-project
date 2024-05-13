e2417ed553c1c8cb9b0e80cb9cd70d7085b0a4a5
Vallejord <burlete@hotmail.com>
2024-05-13 18:14:20 -0300

    property page working

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -3,6 +3,8 @@ import { useEffect, useState } from "react";
 import { useParams } from "next/navigation";
 import { fetchProperty } from "@/utils/request";
 
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+
 const PropertyPage = () => {
   const { id } = useParams();
   const [property, setProperty] = useState(null);
@@ -26,8 +28,22 @@ const PropertyPage = () => {
       fetchPropertyData();
     }
   }, [id, property]);
-
-  return <div>PropertyPage</div>;
+  if (!property && !loading) {
+    return (
+      <h1 className="text-center txt-2xl font-bold mt-10">
+        Property Not Found
+      </h1>
+    );
+  }
+  return (
+    <>
+      {!loading && (
+        <>
+          <PropertyHeaderImage image={property.images[0]} />
+        </>
+      )}
+    </>
+  );
 };
 
 export default PropertyPage;
