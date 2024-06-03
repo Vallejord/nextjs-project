d7175fe8d780809f4c473c4ad21d7c0aa6a8acf3
Ezequiel Vallejo <ezequiel.vallejo@globant.com>
2024-06-03 12:10:18 -0300

    added get session user file

--- app/api/properties/route.js
+++ app/api/properties/route.js
@@ -1,5 +1,7 @@
 import connectDB from "@/config/database";
 import Property from "@/models/Property";
+import { getSessionUser } from "@/utils/getSessionUser";
+import { getSession } from "next-auth/react";
 
 //GET /api/properties
 export const GET = async (request) => {
@@ -21,6 +23,16 @@ export const GET = async (request) => {
 
 export const POST = async (request) => {
   try {
+    await connectDB();
+
+    const sessionUser = await getSessionUser();
+
+    if (!sessionUser || !sessionUser.userId) {
+      return new Response("User ID", { status: 401 });
+    }
+
+    const { userId } = sessionUser;
+
     const formData = await request.formData();
 
     //access all values fro amenities and images , that are arrays
@@ -55,12 +67,22 @@ export const POST = async (request) => {
         email: formData.get("seller_info.email"),
         phone: formData.get("seller_info.phone"),
       },
-      images,
+      owner: userId,
+      // images,
     };
 
-    return new Response(JSON.stringify({ message: "Success" }), {
-      status: 200,
-    });
+    console.log(propertyData);
+
+    const newProperty = new Property(propertyData);
+    await newProperty.save();
+
+    return Response.redirect(
+      `${process.env.NEXTAUTH_URL}/properties/${newProperty._id}}`
+    );
+
+    // return new Response(JSON.stringify({ message: "Success" }), {
+    //   status: 200,
+    // });
   } catch (error) {
     return new Response("Failed to add property", {
       status: 500,
